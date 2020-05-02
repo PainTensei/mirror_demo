c90efc6cddb91a473d41e1992042198e23db2983
Thijs Deenen <thijsdeenen@kpnmail.nl>
2020-05-02 15:45:53 +0200

    Added mouseFollow Object (Brush)

---
+++ mirror_demo/Assets/Scripts/Brush.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour
+{
+    private Vector3 mousePos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z - transform.position.z)));
+        mousePos.z = transform.position.z;
+
+
+        transform.position = mousePos;
+    }
+}
